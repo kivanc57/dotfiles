ed088ace321fecacc0cefec8e344071fefcc2d6e
Kivanc Gordu <kivanc.gordu@hotmail.com>
2025-08-26 21:12:41 +0200

    change: lexima rule of '<>' only for lua, html & xml

--- nvim/.config/nvim/lua/plugins/minipairs.lua
+++ nvim/.config/nvim/lua/plugins/minipairs.lua
@@ -3,6 +3,12 @@ return {
   config = function ()
     vim.g.lexima_enable_basic_rules = 1
     vim.g.lexima_enable_newline_rules = 1
-    vim.fn['lexima#add_rule'] ({char='<', input_after='>'})
+
+
+    vim.fn['lexima#add_rule'] ({
+        char = '<',
+        input_after = '>',
+        filetype = {'lua', 'html', 'xml'}
+    })
   end
 }
