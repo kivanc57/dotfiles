d33c1fa9074c286c4eda9bf28058c2375d8de386
Kivanc Gordu <kivanc.gordu@hotmail.com>
2025-08-31 20:02:07 +0200

    add: hyprsunset and its script

---
+++ custom-scripts/.local/bin/night-mode.sh
@@ -0,0 +1,7 @@
+if pgrep -x "hyprsunset" > /dev/null; then
+    killall -9 hyprsunset
+    notify-send "Night Light" "Off" -u "low"
+else
+    hyprsunset --temperature 4000 &
+    notify-send "Night Light" "On" -u "low"
+fi
