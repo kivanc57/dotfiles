0995ce9e50c8d935b8ba7d8e4960ee8e16df2f8f
Kivanc Gordu <kivanc.gordu@hotmail.com>
2025-09-01 16:53:41 +0200

    change: temperature of night-mode.sh 4000 -> 2000

--- custom-scripts/.local/bin/night-mode.sh
+++ custom-scripts/.local/bin/night-mode.sh
@@ -2,6 +2,6 @@ if pgrep -x "hyprsunset" > /dev/null; then
     killall -9 hyprsunset
     notify-send "Night Light" "Off" -u "low"
 else
-    hyprsunset --temperature 4000 &
+    hyprsunset --temperature 2000 &
     notify-send "Night Light" "On" -u "low"
 fi
