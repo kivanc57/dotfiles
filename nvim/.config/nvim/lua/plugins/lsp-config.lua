81102f03c48785af2b7b641b3d39fb81390b23c6
Kivanc Gordu <kivancgordu@hotmail.com>
2026-07-22 14:36:16 +0200

    feat(which-key): add which-key and description for keymaps

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -42,12 +42,14 @@ return {
 
       vim.lsp.enable(servers)
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go Definition" })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "References" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format" })
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+
     end,
   },
 }
